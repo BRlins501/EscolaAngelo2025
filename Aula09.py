c972c6be06a9a1c6d524bcdc458e99dcbccba758
BRlins501 <brunim317@gmail.com>
2025-03-18 08:26:36 -0300

    aula

---
+++ Aula09.py
@@ -0,0 +1,12 @@
+# Crie duas variaveis e atribua valores inteiros
+# divida o maior pelo menor e verifique se o resto
+# da divisão deu zero. Se deu zero imprima
+# "O número maior par",senão imprima
+# "O maior é impar". Use divisão //, para o resto use %
+
+numero = 15
+
+if numero % 2 ==0:
+    print("O número maior par")
+else: 
+    print("O maior é impar")
