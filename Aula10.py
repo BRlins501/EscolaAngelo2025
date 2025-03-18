35e0e7a82d3cbc1d650ca75f26b2e1ee91cd60c7
BRlins501 <brunim317@gmail.com>
2025-03-18 08:31:34 -0300

    aula

---
+++ Aula10.py
@@ -0,0 +1,14 @@
+v1 = "escola"
+v2 = "trabalho"
+v3 = "parque"
+
+escola = v2
+if escola == " escola":
+    print(" Vá para a escola!")
+elif escola == "trabalho":
+    print("Vá para o trabalho")
+elif escola == "parque":
+    print("Vá para o parque")
+else:
+    print("Nenhuma esola válida")
+    
