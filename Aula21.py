33ae0b6288b141fe3114617330d9ac87fda16919
BRlins501 <brunim317@gmail.com>
2025-03-18 07:45:51 -0300

    aula

---
+++ Aula21.py
@@ -0,0 +1,18 @@
+# EXERCÍCIOS DE FIXAÇÃO
+
+# 1. adicione o número 50 ao final da lista
+lista_numero = [10, 20, 30, 40]
+lista_numero.append(50)
+print(lista_numero)
+
+# 2. Adicione "laranja" ao índice 1 na lista
+lista_fruta = ["maça", "banana", "uva"]
+lista_fruta.insert(1, "laranja")
+print(lista_fruta)
+
+
+# 3. remova "cachorro" da lista
+lista_animais = ["cachorro", "gato", "pássaro", "cachorro"]
+lista_animais.remove("cachorro")
+print(lista_animais)
+
