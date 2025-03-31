b7ffadcdaf0d7e43f3df865f5b05be62bf5feee1
BRlins501 <brunim317@gmail.com>
2025-03-31 07:40:06 -0300

    Aula

--- Aula26.py
+++ Aula26.py
@@ -2,27 +2,27 @@
 
 #  a) Peça ao usuário para digitar seu nome completo.
 nome_completo = input("Digite seu nome completo: ")
-print("Olá, " + nome_completo)
 
 #  b) Concatene “Olá,” com o nome fornecido e exiba o resultado.
-nome_completo = input("Digite seu nome completo: ")
-print("Olá, " + nome_completo)
+nome_completo = "Olá, " + nome_completo
+print(nome_completo)
 
 #  c) Conte quantos caracteres existem no nome completo digitado e exiba o resultado.
-print("O nome completo tem", len(nome_completo), "caracteres.")
+caracteres = len(nome_completo)
+print(f"Seu nome completo tem {caracteres} caracteres")
+
 #  d) Peça ao usuário para digitar um sobrenome para procurar na string do nome completo.
-print("O nome completo tem", len(nome_completo), "caracteres.")
+sobrenome = input("Digite um sobrenome para procurar no seu nome completo: ")
 
 #  e) Verifique se o sobrenome fornecido está presente no nome completo e exiba uma mensagem apropriada.
-sobrenome = input("Digite um sobrenome para procurar: ")
 if sobrenome in nome_completo:
-  print("O sobrenome foi encontrado no nome completo.")
+  print(f"O sobrenome {sobrenome} está presente no nome completo.")
 else:
-  print("O sobrenome não foi encontrado no nome completo.")
+  print(f"O sobrenome {sobrenome} não esta presente no nome completo.")
 
 #  f) Exiba o nome completo em letras maiúsculas.
-print(nome_completo.upper())
+print("Seu nome completo em letras minusculas é", nome_completo.upper())
 
 #  g) Substitua todas as ocorrências da vogal “a” na string do nome completo pelo caractere ‘4’ e exiba o resultado
-nome_completo_modificado = nome_completo.replace("a", "4")
-print(nome_completo_modificado)
+nome_completo = nome_completo.replace("a", "4")
+print("Nome completo com 'a'  substituidopor '4': ", nome_completo)
