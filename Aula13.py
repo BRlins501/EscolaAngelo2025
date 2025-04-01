fbb0f5020b03cacbb64756e539c2dc316969cf43
BRlins501 <brunim317@gmail.com>
2025-04-01 08:00:54 -0300

    Aula

--- Aula13.py
+++ Aula13.py
@@ -1,14 +1,14 @@
 valor = int(input("Digiote u8m enteiro entr4e 10 e 100"))
 
 if valor <= 10 or valor >= 100:
- print("valor inválido")
+   print("valor inválido")
 else:
-    if valor % 2 == 0:
-        if valor == 50:
+   if valor % 2 == 0:
+      if valor == 50:
            print("Você digitou 50")
-        else:
+      else:
            print("Você não digitou 50")
-    else:
+   else:
       print("O valor é impar")
 
 
