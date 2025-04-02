27b062fe9dfbc2cfd76e0c85fec48ca6443af8bf
BRlins501 <brunim317@gmail.com>
2025-04-02 09:28:52 -0300

    Aula

---
+++ Aula29.py
@@ -0,0 +1,19 @@
+# TUPLA x LISTA
+
+lista_vazia = []
+print(lista_vazia)
+lista_vazia.append("Bom Dia")
+lista_vazia.append("Olá, mundo")
+print(lista_vazia)
+
+tupla = ()
+print(tupla)
+tupla = list(("Ana", "Paula", "Sofia"))
+tupla.append ("Cristal")
+tupla = tuple(tupla)
+print(tupla)
+
+print(lista_vazia[0])
+print(tupla[3])
+
+
